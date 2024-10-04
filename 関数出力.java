b5a64126c51b5cc5fe38b29991fb8ee6d379c857
xingyanjintian <xingyanjintian@gmail.com>
2024-10-04 22:44:59 +0900

    関数helloを作成

--- 関数出力.java
+++ 関数出力.java
@@ -1,4 +1,7 @@
 public class 関数出力 {
+    public static void hello(){//関数helloを作成
+        System.out.println("Hello world");
+    }
     public static void main(String[] args) {
         System.out.println("Hello git");    
         

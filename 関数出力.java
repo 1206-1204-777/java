9bd65181f2bec956af180dc0bccc1b0965507221
xingyanjintian <xingyanjintian@gmail.com>
2024-10-04 23:38:23 +0900

    値段の合計を出力させる

--- 関数出力.java
+++ 関数出力.java
@@ -1,16 +1,26 @@
 public class 関数出力 {
-    public static void hello(){//関数helloを作成
+    public static void hello(){                 //関数helloを作成
         System.out.println("Hello world");
     }
-    public static void twice(int x) {//関数twiceを作成
-        System.out.println(x*2);     //２倍した値を出力   
+    public static void twice(int x) {           //関数twiceを作成
+        System.out.println(x*2);                //２倍した値を出力   
+    }
+    public static void math(int x,int y) {      //math関数を作成
+        System.out.println(x+y);                //appleとorangeの値段の合計を出力
     }
     public static void main(String[] args) {
         System.out.println("Hello git");
-        hello();//hello関数を呼び出し
-        System.out.println();//改行用
-        
+        hello();                                //hello関数を呼び出し
+        System.out.println();                   //改行用
+
         int num =50;
-        twice(num);//関数twiceを呼び出す
+        twice(num);                             //関数twiceを呼び出す
+        System.out.println();                   //改行
+
+        //複数の関数を使用
+        int apple, orange;
+        apple = 100;
+        orange = 200;                           //apple,orangeの値段を設定
+        math(apple,orange);                     //math関数を呼び出し
     }
 }

9cee1274eee4bc0d7ba9e2f32fa80ab7ad82e232
xingyanjintian <xingyanjintian@gmail.com>
2024-10-05 00:34:03 +0900

    戻り値を使用し処理を実行

--- 関数出力.java
+++ 関数出力.java
@@ -5,8 +5,12 @@ public class 関数出力 {
     public static void twice(int x) {           //関数twiceを作成
         System.out.println(x*2);                //２倍した値を出力   
     }
-    public static void math(int x,int y) {      //math関数を作成
-        System.out.println(x+y);                //appleとorangeの値段の合計を出力
+    public static int math(int x,int y) {      //math関数を作成
+
+        int total =x * 10;                     //total変数を作成
+        return total;                          //returnで戻り値をmain関数に返す
+
+        //System.out.println(x+y);             //appleとorangeの値段の合計を出力
     }
     public static void main(String[] args) {
         System.out.println("Hello git");
@@ -23,6 +27,7 @@ public class 関数出力 {
         orange = 200;                           //apple,orangeの値段を設定
         math(apple,orange);                     //math関数を呼び出し
         //戻り値
-        
+        int result = math(apple, orange);       //戻り値を指定
+        System.out.println(result);
     }
 }

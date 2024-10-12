76e0c26bc6ab77b65e0d2c250d1f1fc81e2c2fb0
xingyanjintian <xingyanjintian@gmail.com>
2024-10-12 19:39:39 +0900

    変数と配列の違いを比較

--- 配列復習.java
+++ 配列復習.java
@@ -1,5 +1,14 @@
 public class 配列復習 {
     public static void main(String[] args) {
-        
+        //変数と配列の違い
+        String test = "Hello ";
+        String test2 = "wold";
+        System.out.println(test+test2);     //変数を使用した出力
+
+        System.out.println();
+
+        String [] word = {"java","mine"};   //配列の作成
+        System.out.println(test+word[0]);   //変数と配列を組み合わせて出力    
+
     }
 }

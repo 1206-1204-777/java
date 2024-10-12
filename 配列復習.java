044225ff0ae5154b5e7968371f33578ac1e79c29
xingyanjintian <xingyanjintian@gmail.com>
2024-10-12 19:53:54 +0900

    配列の要素の追加方法を復習

--- 配列復習.java
+++ 配列復習.java
@@ -9,6 +9,17 @@ public class 配列復習 {
 
         String [] word = {"java","mine"};   //配列の作成
         System.out.println(test+word[0]);   //変数と配列を組み合わせて出力    
+        System.out.println();
 
+        System.out.println("配列の取り出し方");
+
+        String [] name;                     //配列nameを作成
+        name = new String[2];               //nameの要素数を指定
+        name[0] = "佐藤";                   
+        name[1] = "加藤";                   //配列の要素を２つ追加
+
+        int num =1;                         //変数を使用して取り出す要素番号を指定
+        System.out.println(name[num]);      //指定した要素を出力
     }
+    
 }

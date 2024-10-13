68e17c3baf1dc422546a6e2e43b6a05c6726db8d
xingyanjintian <xingyanjintian@gmail.com>
2024-10-13 21:14:26 +0900

    文字列Helloを出力 する関数を作成

--- 関数入力.java
+++
@@ -1,5 +0,0 @@
-public class 関数入力 {
-    public static void hello2(String[] args) {
-        System.out.println("Hello ");
-    }
-}
--- 関数復習.java
+++ 関数復習.java
@@ -1,5 +1,8 @@
 public class 関数復習 {
+    public static void hello2() {
+        System.out.println("Hello ");
+    }
     public static void main(String[] args) {
-        
+        hello2();
     }
 }

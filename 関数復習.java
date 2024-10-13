af72d09acfbc04010f41d02eb145dd405186e0e3
xingyanjintian <xingyanjintian@gmail.com>
2024-10-13 21:59:28 +0900

    引数が設定された関数mathを作成し出力

--- 関数復習.java
+++ 関数復習.java
@@ -1,8 +1,16 @@
 public class 関数復習 {
-    public static void hello() {
+    public static void hello() {            //引数の無い関数
         System.out.println("Hello ");
     }
+
+    public static void math(int x) {        //引数(int x)を指定
+        System.out.println(x * 10);
+    }
     public static void main(String[] args) {
+        System.out.println("仮引数なし");
         hello();
+        System.out.println("仮引数あり");
+        int num =10;
+        math(num);
     }
 }

74073289c0fbb332548edc798c57d9875d1c8e82
xingyanjintian <xingyanjintian@gmail.com>
2024-10-14 22:09:45 +0900

    入力された値を配列に格納する方法を練習

--- 標準入力.java
+++ 標準入力.java
@@ -14,5 +14,17 @@ class 標準入力{
             System.out.println("Hello "+data);  //取得した変数dataを出力
         }
         //標準入力したデータを配列に格納する
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(n + m);
+
+        int[][] table = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                table[i][j] = sc.nextInt();
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-} 
+}  

1b489f9a8895fd46031a0211c8a9db6af6a66586
xingyanjintian <xingyanjintian@gmail.com>
2024-10-14 12:13:26 +0900

    番号にに対応した各要素を出力

--- 二次元配列.java
+++ 二次元配列.java
@@ -24,13 +24,14 @@ public class 二次元配列 {
         System.out.println("ループでの処理方法");           //ループを使用し各要素の番号を改行して出力
         for(int i = 0; i < teams.length; i++) {
             for (int j = 0; j < teams[i].length; j++) {
-                System.out.print(i);
+                System.out.print(teams[i][j]);              //番号にに対応した各要素を出力
+                System.out.print(i+"-");
                 System.out.print(j);
                 System.out.print(" ");
             }
             System.out.println("");
             System.out.println("----------");
         }
-        
+
     }
 }

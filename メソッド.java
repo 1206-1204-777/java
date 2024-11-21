20b21fd837a2c3b3a2f15a6c8330cbb2ba17aff8
xingyanjintian <xingyanjintian@gmail.com>
2024-11-21 16:33:44 +0900

    戻り値を使用して計算するmathメソッドを作成

--- メソッド.java
+++ メソッド.java
@@ -2,8 +2,14 @@ public class メソッド{
 public static void main(String[] args) {  //mainメソッドを作成
     System.out.println("Hello world");  //mainメソッドの内容
     sayHello();                           //sayHelloメソッドを呼び出す。内容はメインと同じ
+    int num1 = math(3);                 //int num1変数を作成。変数内容はmathメソッドに３を渡す
+    System.out.println(num1);             //mathメソッドから返ってきた値を出力する
 }
 public static void sayHello(){
     System.out.println("Hello world");  //sayHelloメソッドを作成
 }
+
+public static int math(int x) {               //mathメソッドを作成
+    return  x * 100;                          //戻り値をx*100としてmainに返す
 }
+} 

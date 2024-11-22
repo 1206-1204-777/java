e646ee680336362893002a17c510b84f7ff3d7f7
xingyanjintian <xingyanjintian@gmail.com>
2024-11-22 17:22:57 +0900

    練習用のコードを記述

--- メソッド2.java
+++ メソッド2.java
@@ -1,5 +1,17 @@
 public class メソッド2 {
     public static void main(String[] args) {
+        Player player = new Player("man");
+        player.work();
+    }
+}
 
+class  Player{
+    private final String name;
+    
+    public Player(String myName){
+        name = myName;
+    }
+    public void work(){
+        System.out.println(name);
     }
 }

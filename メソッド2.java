4c8fc68cd8c2995c5c37e5b0f1a7c34c19be91c8
xingyanjintian <xingyanjintian@gmail.com>
2024-11-22 16:16:12 +0900

    メソッドのテストを作成

--- メソッド2.java
+++ メソッド2.java
@@ -1,5 +1,22 @@
 public class メソッド2 {
     public static void main(String[] args) {
+        attack("monster");
+        Enemy enemy = new Enemy("モンスター");
+        enemy.enemy();
         
+    }
+    public static void attack(String enemy) {
+        System.out.println(enemy);
+    }
+}
+
+class Enemy{
+    private final String enemyName;
+
+    public Enemy(String name){
+        enemyName = name;
+    }
+    public void enemy() {
+        System.out.println(enemyName);
     }
 }

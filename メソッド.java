9c02a1f26882e1e5cc43902d4fdc6858eff86d96
xingyanjintian <xingyanjintian@gmail.com>
2024-11-22 13:04:01 +0900

    Msgクラスを作成

--- メソッド.java
+++ メソッド.java
@@ -4,6 +4,9 @@ public static void main(String[] args) {  //mainメソッドを作成
     sayHello();                           //sayHelloメソッドを呼び出す。内容はメインと同じ
     int num1 = math(3,2);                 //int num1変数を作成。変数内容はmathメソッドに３を渡す
     System.out.println(num1);             //mathメソッドから返ってきた値を出力する
+
+    Msg msg1 = new Msg();
+    msg1.msg();
 }
 public static void sayHello(){
     System.out.println("Hello world");  //sayHelloメソッドを作成
@@ -13,3 +16,9 @@ public static int math(int x,int y) {               //mathメソッドを作成
     return  x * y;                          //戻り値をx*yとしてmainに返す
 }
 } 
+
+class Msg{                                  //Msgクラスを作成
+    public void msg(){
+        System.out.println("I walk to day");
+    }
+}

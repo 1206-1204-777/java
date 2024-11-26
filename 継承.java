55c687dc5af7cf5cd91cc717dcba2f30ce8b16b3
xingyanjintian <xingyanjintian@gmail.com>
2024-11-26 19:57:42 +0900

    継承クラスの子クラス(sub)を作成

--- 継承.java
+++ 継承.java
@@ -1,5 +1,14 @@
 public class 継承 {
     public static void main(String[] args) {
         System.out.println("hello");
+        sub sub = new sub();
+        sub.look();
+        
+    }
+}
+@SuppressWarnings("unused")
+class sub extends 継承{
+    public void look(){
+        System.out.println("world");
     }
 }

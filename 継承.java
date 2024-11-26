f944eba8a6a41cbc6c44ad53df202955636981dc
xingyanjintian <xingyanjintian@gmail.com>
2024-11-26 20:05:00 +0900

    継承クラスにtestメソッドを作成し呼び出し

--- 継承.java
+++ 継承.java
@@ -1,14 +1,19 @@
 public class 継承 {
     public static void main(String[] args) {
-        System.out.println("hello");
-        sub sub = new sub();
-        sub.look();
-        
+        System.out.print("hello");
+        sub sub = new sub();                //subオブジェクトを作成
+        sub.look();                         //subクラスのlookメソッドを呼び出し
+        sub.test();                         //subに継承した継承クラスのtestメソッドを呼び出し
+    }
+    public void test(){
+        int x = 50;
+        System.out.println(x*2);
     }
 }
 @SuppressWarnings("unused")
 class sub extends 継承{
     public void look(){
-        System.out.println("world");
+        System.out.println(" world");
+        System.out.println("こんにちは！");
     }
 }

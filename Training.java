e4b4525a652ae5db73b3e5068275099776343031
xingyanjintian <xingyanjintian@gmail.com>
2024-11-27 19:06:34 +0900

    引数のあるmoveメソッドと引数のないmoveメソッドをそれぞれ設定し呼び出す

--- Training.java
+++ Training.java
@@ -1,5 +1,12 @@
 public class Training {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        move();
+        move("work");                       //引数アリのメソッドmoveを呼び出し
+    }
+    public static void move() {
+        System.out.println("hello");
+    }
+    public static void move(String My_move) {      //引数 My_moveを指定し出力するメソッド
+        System.out.println(My_move);
     }
 }

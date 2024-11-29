ec9ac2fea8a46f6c232ffcc14ccd59384384fd45
xingyanjintian <xingyanjintian@gmail.com>
2024-11-29 20:17:17 +0900

    例外処理を記述して例外が発生した場合の処理を記述

--- Exception.java
+++ Exception.java
@@ -1,5 +1,16 @@
 public class Exception{
     public static void main(String[] args) {
+        System.out.println("Hello world");
+        //例外処理の記述
+        try {                                         //例外があった場合に補足する範囲を決めてその処理を実行する  
+            int num = 0;
+        int answer = 100 / num;         
+        System.out.println(answer);                         
+        } catch (ArithmeticException e){            //エラーが発生した場合のメッセージを出力する
+            System.out.println("error "+ e.getMessage());
+        }finally {                                  //例外の有無にかかわらず処理をする
+            System.out.println("java");
+        }
         
     }
 }

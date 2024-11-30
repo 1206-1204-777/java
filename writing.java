96c791ff81db957d958fd2b7d7a0eff1a6ec205c
xingyanjintian <xingyanjintian@gmail.com>
2024-11-30 21:03:06 +0900

    メッセージなどの一部インデントを修正

--- writing.java
+++ writing.java
@@ -4,19 +4,24 @@ public class writing {
     public static void main(String[] args) throws IOException{
         //ファイルの作成
         File file = new  File("articles.txt");   //作成と書きこむファイルの指定を行う
-        try ( //ファイルへの書き込み
-                FileWriter writing = new FileWriter(file)) {
+        //ファイルへの書き込み
+        try (FileWriter writing = new FileWriter(file)) {
                 writing.write("Hello\n");
                 writing.write("java\n");
                 writing.write("こんにちは");
 
+                for (int i = 0; i < 100+1; i++) {        //forで１から１００までの数を書きこむ処理
+
+                    writing.write(i+"\n");
+                }
+
                 //ファイルの読み込み
-                Scanner sc = new Scanner(file);         //ファイルを指定して読み込む
+            try (Scanner sc = new Scanner(file)) {//ファイルを指定して読み込む
                 while (sc.hasNextLine()) {
                     boolean Line =  sc.hasNextLine();
                     System.out.println(Line);
                 }
-                sc.close();
+            }
         }
     }
 }
